9ad54877497dadf3cdf68c8672066aa6207b1ec3
Dawid Blachowski <dawid@Nyreen.lan>
2024-02-27 00:36:10 +0100

    add datagrid, category and links to products

--- PROJEKT/src/components/product.jsx
+++ PROJEKT/src/components/product.jsx
@@ -1,8 +1,16 @@
-function Product({ name, price }) {
+import DataGrid from "./datagrid";
+
+function Product({ name, price, productURL, other, imgURL, category }) {
     return (
         <div className='product'>
-            <h2>{name}</h2>
+            <a href={productURL}>
+                <img src={imgURL} alt="" />
+                <h2>{name}</h2>
+            </a>
             <p>Price: {price}</p>
+            <p>Category: {category}</p>
+            <DataGrid arr={other} />
+            <hr />
         </div>
     );
 }
--- PROJEKT/src/components/productList.jsx
+++ PROJEKT/src/components/productList.jsx
@@ -4,7 +4,7 @@ function ProductList({ products }) {
     return (
         <div className="products">
             {products.map((product, index) => (
-                <Product key={index} name={product.name} price={product.price} />
+                <Product key={index} name={product.name} price={product.price} productURL={product.productURL} other={product.other} imgURL={product.imgURL} category={product.category}/>
             ))}
         </div>
     );
