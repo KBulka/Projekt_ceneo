489eef1bb8b533239879e400f1977f0f7b0833a4
Łukasz Pezda <pezda-lukasz@o2.pl>
2024-02-26 16:18:58 +0100

    crude searchbar

--- PROJEKT/src/App.jsx
+++ PROJEKT/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function makeLink() {
+        const inputText = document.getElementById('searchBar').value;
+        return "www.ceneo.pl/;szukaj-" + inputText
+    }
+
+    return(
+        <>
+            <h1>Wyszukaj swój produkt</h1>
+            <input type="text" name="searchBar" id="searchBar" />
+            <button onClick={makeLink}>Szukaj</button>
+        </>
+    )
 }
 
 export default App
