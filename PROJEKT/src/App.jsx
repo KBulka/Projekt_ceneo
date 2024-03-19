fdb4edc7a6d8da0876c120634471b97e9d62b1d7
Łukasz Pezda <pezda-lukasz@o2.pl>
2024-03-19 18:28:49 +0100

    small css changes

--- PROJEKT/src/App.jsx
+++ PROJEKT/src/App.jsx
@@ -1,113 +1,145 @@
-import './App.css';
-import { useState } from 'react';
-import ProductList from './components/productList';
+import "./App.css";
+import { useState } from "react";
+import ProductList from "./components/productList";
 
 function App() {
-    const [searchInput, setSearchInput] = useState('');
-    const [products, setProducts] = useState([]);
-    const [activeButton, setActiveButton] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [products, setProducts] = useState([]);
+  const [activeButton, setActiveButton] = useState(null);
 
-    const fetchData = (event) => {
-        event.preventDefault()
-        fetch('http://localhost:3000/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ searchInput }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            // const productsWithFloatPrice = data.map(product => {
-            //     const priceWithoutSpaces = product.price.replace(/\s/g, '');
-            //     const priceWithDot = priceWithoutSpaces.replace(',', '.');
-            //     const parsedPrice = parseFloat(priceWithDot).toFixed(2);
-            //     return {
-            //         ...product,
-            //         price: parsedPrice
-            //     };
-            // });
-
-            const producstWithDate = data.map(product => {
-                const createdAt = new Date(product.createdAt).toLocaleDateString("pl-PL");
-                const updatedAt = new Date(product.updatedAt).toLocaleDateString("pl-PL");
-                return {
-                    ...product,
-                    createdAt,
-                    updatedAt
-                };
-            });
-
-
-            setProducts(producstWithDate);
-            console.log(data)
-            // console.log(productsWithFloatPrice)
-        })
-        .catch(error => {
-            console.error('Error:', error);
+  const fetchData = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ searchInput }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const producstWithDate = data.map((product) => {
+          const createdAt = new Date(product.createdAt).toLocaleDateString(
+            "pl-PL"
+          );
+          const updatedAt = new Date(product.updatedAt).toLocaleDateString(
+            "pl-PL"
+          );
+          return {
+            ...product,
+            createdAt,
+            updatedAt,
+          };
         });
-    };
 
-    const handleInputChange = event => {
-        setSearchInput(event.target.value);
-    };
+        setProducts(producstWithDate);
+        console.log(data);
+        // console.log(productsWithFloatPrice)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
+  const handleInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
 
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
 
-    const handleButtonClick = (buttonName) => {
-      setActiveButton(buttonName);
-    };
-
-    const sortByName = () => {
-        const sortedProducts = [...products].sort((a, b) => a.name.localeCompare(b.name));
-        setProducts(sortedProducts);
-        handleButtonClick('sortByName');
-      };
-
-      const sortByNameReverse = () => {
-        const sortedProducts = [...products].sort((a, b) => b.name.localeCompare(a.name));
-        setProducts(sortedProducts);
-        handleButtonClick('sortByNameReverse');
-      };
-
-      const sortByPriceAscending = () => {
-        const sortedProducts = [...products].sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-        setProducts(sortedProducts);
-        handleButtonClick('sortByPriceAscending');
-      };
-
-      const sortByPriceDescending = () => {
-        const sortedProducts = [...products].sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
-        setProducts(sortedProducts);
-        handleButtonClick('sortByPriceDescending');
-      };
-
-    return (
-        <>
-            <form onSubmit={fetchData}>
-                <label htmlFor="searchBar">Wyszukaj swój produkt</label>
-                <input
-                type="text"
-                name="searchBar"
-                id="searchBar"
-                placeholder="Wpisz nazwę produktu"
-                value={searchInput}
-                onChange={handleInputChange}
-                />
-                <input id='searchBtn' type="submit" value="Szukaj" />
-            </form>
-
-            <div className="sortingOptions">
-                <button className={activeButton === 'sortByName' ? 'active' : ''} onClick={sortByName}>Sortuj A-Z</button>
-                <button className={activeButton === 'sortByNameReverse' ? 'active' : ''} onClick={sortByNameReverse}>Sortuj Z-A</button>
-                <button className={activeButton === 'sortByPriceAscending' ? 'active' : ''} onClick={sortByPriceAscending}>Cena rosnąco</button>
-                <button className={activeButton === 'sortByPriceDescending' ? 'active' : ''} onClick={sortByPriceDescending}>Cena malejąco</button>
-            </div>
-
-            <ProductList products={products} />
-        </>
+  const sortByName = () => {
+    const sortedProducts = [...products].sort((a, b) =>
+      a.name.localeCompare(b.name)
     );
+    setProducts(sortedProducts);
+    handleButtonClick("sortByName");
+  };
+
+  const sortByNameReverse = () => {
+    const sortedProducts = [...products].sort((a, b) =>
+      b.name.localeCompare(a.name)
+    );
+    setProducts(sortedProducts);
+    handleButtonClick("sortByNameReverse");
+  };
+
+  const sortByPriceAscending = () => {
+    const sortedProducts = [...products].sort(
+      (a, b) => parseFloat(a.price) - parseFloat(b.price)
+    );
+    setProducts(sortedProducts);
+    handleButtonClick("sortByPriceAscending");
+  };
+
+  const sortByPriceDescending = () => {
+    const sortedProducts = [...products].sort(
+      (a, b) => parseFloat(b.price) - parseFloat(a.price)
+    );
+    setProducts(sortedProducts);
+    handleButtonClick("sortByPriceDescending");
+  };
+
+  return (
+    <>
+      <form>
+        <label htmlFor="searchBar">Wyszukaj swój produkt</label>
+        <input
+          type="text"
+          name="searchBar"
+          id="searchBar"
+          placeholder="Wpisz nazwę produktu"
+          value={searchInput}
+          onChange={handleInputChange}
+        />
+        <input
+          onClick={fetchData}
+          className="searchBtn"
+          id="searchBtnDB"
+          type="button"
+          value="Pobierz dane z bazy danych"
+        />
+        <input
+          onClick={fetchData} // Tutaj do zmienienia na funcje pobierająca dane z ceneo
+          className="searchBtn"
+          id="searchBtnCeneo"
+          type="button"
+          value="Pobierz dane z Ceneo"
+        />
+      </form>
+
+      <div className="sortingOptions">
+        <button
+          className={activeButton === "sortByName" ? "active" : ""}
+          onClick={sortByName}
+        >
+          Sortuj A-Z
+        </button>
+        <button
+          className={activeButton === "sortByNameReverse" ? "active" : ""}
+          onClick={sortByNameReverse}
+        >
+          Sortuj Z-A
+        </button>
+        <button
+          className={activeButton === "sortByPriceAscending" ? "active" : ""}
+          onClick={sortByPriceAscending}
+        >
+          Cena rosnąco
+        </button>
+        <button
+          className={activeButton === "sortByPriceDescending" ? "active" : ""}
+          onClick={sortByPriceDescending}
+        >
+          Cena malejąco
+        </button>
+        <button>Next</button>
+      </div>
+
+      <ProductList products={products} />
+    </>
+  );
 }
 
 export default App;
-
--- PROJEKT/src/components/productList.jsx
+++ PROJEKT/src/components/productList.jsx
@@ -1,13 +1,22 @@
-import Product from './product';
+import Product from "./product";
 
 function ProductList({ products }) {
-    return (
-        <div className="products">
-            {products.map((product, index) => (
-                <Product key={index} name={product.name} price={product.price} productURL={product.productURL} imgURL={product.imgURL} category={product.category} createdAt={product.createdAt} updatedAt={product.updatedAt}/>
-            ))}
-        </div>
-    );
+  return (
+    <div className="products">
+      {/* <button>Następna strona</button> */}
+      {products.map((product, index) => (
+        <Product
+          key={index}
+          name={product.name}
+          price={product.price}
+          productURL={product.productURL}
+          imgURL={product.imgURL}
+          createdAt={product.createdAt}
+          updatedAt={product.updatedAt}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ProductList;
